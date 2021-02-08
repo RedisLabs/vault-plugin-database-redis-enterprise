fb90b2bf3efb496decd8d38ad2a15519594ac097
Trent Rosenbaum <trent.j.rosenbaum@gmail.com>
2021-02-08 15:48:20 +0000

    Allows the fixtures to be disbled through RS_DISABLE_FIXTURES env variable

--- test_utils.go
+++ test_utils.go
@@ -2,20 +2,34 @@ package vault_plugin_database_redisenterprise
 
 import (
 	"crypto/tls"
+	"fmt"
 	"github.com/dnaeon/go-vcr/cassette"
 	"github.com/dnaeon/go-vcr/recorder"
 	"github.com/stretchr/testify/require"
 	"net/http"
+	"os"
+	"strconv"
 	"testing"
 )
 
-func record(t *testing.T, fixture string, f func(*testing.T, *recorder.Recorder)) {
-	r, err := recorder.New("fixtures/" + fixture)
-	require.NoError(t, err)
+var(
+	disbaleFixtures = getEnvAsBool("RS_DISABLE_FIXTURES", false)
+)
 
-	r.SetTransport(&http.Transport{
+func record(t *testing.T, fixture string, f func(*testing.T, *recorder.Recorder)) {
+
+	cassetteName := fmt.Sprintf("fixtures/%s", fixture)
+
+	fixtureMode := recorder.ModeReplaying
+	if disbaleFixtures {
+		fixtureMode = recorder.ModeDisabled
+	}
+
+	r, err := recorder.NewAsMode(cassetteName, fixtureMode, &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	})
+	require.NoError(t, err)
+
 	r.AddFilter(func(i *cassette.Interaction) error {
 		delete(i.Request.Headers, "Authorization")
 		return nil
@@ -27,4 +41,21 @@ func record(t *testing.T, fixture string, f func(*testing.T, *recorder.Recorder)
 	}()
 
 	f(t, r)
+}
+
+func getEnv(key string, defaultVal string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+
+	return defaultVal
+}
+
+func getEnvAsBool(name string, defaultVal bool) bool {
+	valStr := getEnv(name, "")
+	if val, err := strconv.ParseBool(valStr); err == nil {
+		return val
+	}
+
+	return defaultVal
 }
